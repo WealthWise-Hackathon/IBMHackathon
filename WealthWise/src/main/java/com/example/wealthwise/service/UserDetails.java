b1403048ee1df24e9ee5b17df37fca31c3da0269
Simon <sd611@student.le.ac.uk>
2025-02-08 13:58:45 +0000

    edit login process

--- WealthWise/src/main/java/com/example/wealthwise/service/UserDetails.java
+++ WealthWise/src/main/java/com/example/wealthwise/service/UserDetails.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-//comment
+//com
 @Service
 public class UserDetails implements UserDetailsService {
     private UserRepository userRepository;
